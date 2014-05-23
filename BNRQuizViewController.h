602afc3c8d472341144c5a40aa44e63b5015b7b1
Izureru <Random>
2014-05-23 21:02:33 +0100

    created the view controller

---
+++ BNRQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRQuizViewController.h
+//  Quiz
+//
+//  Created by I Lewis on 23/05/2014.
+//  Copyright (c) 2014 Jream Matter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRQuizViewController : UIViewController
+
+@end
